962a4e69916d6b8c875c0fba40f0abd5c87cf1c0
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2018-09-21 22:44:19 -0500

    Switch to generic type
    not ROGZIS

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import re
 
 
-def ZGenerate(FullFile, Data):
+def ZGenerate(FullFile):
     # ZScript Generation
     print("Generating ZScript")
 
@@ -25,77 +25,27 @@ def ZGenerate(FullFile, Data):
                         continue
                     Call += "+" if Default[1] == "true" else "-"
                     if SectionName.find(".") > -1:
-                        Call += SectionName.split(".")[1] + "."
-                    Call += Default[0] + ";"
+                        Call += SectionName.split(".")[1]+"."
+                    Call += Default[0]+";"
                 else:
                     if SectionName == "Type":
-                        Call += Default + ";"
+                        Call += Default+";"
                     else:
                         Default = Default.split("=")
                         if len(Default) < 2:
                             continue
                         if SectionName == "Info":
-                            Call += "//$" + Default[0] + " " + Default[1] + "\n"
+                            Call += "//$"+Default[0]+" "+Default[1]+"\n"
                         else:
                             if not SectionName == "Default":
-                                Call += SectionName + "."
-                            Call += (
-                                Default[0] + " " + re.sub("[()]", "", Default[1]) + ";"
-                            )
-        FullFile = Pattern.sub(Call + "}", FullFile, 1)
+                                Call += SectionName+"."
+                            Call += Default[0]+" " + \
+                                re.sub("[()]", "", Default[1])+";"
+        FullFile = Pattern.sub(Call+"}", FullFile, 1)
         Call = Pattern.search(FullFile)
     print("Successful")
 
-    # Upgrades
-    print("  Upgrades:")
-
-    # Marine Armor
-    print("    Marine:")
-    MAUpgrades = []
-    for MAUpgrade in Data["DATABASE"].execute("SELECT * FROM MarineArmorUpgrades"):
-        ClassName = "ZMAU_{}".format(MAUpgrade[0])
-        MAUpgrades.append(ClassName)
-        print("      {}".format(ClassName))
-        if MAUpgrade[2] == None:
-            MAUpgrade[2] = 0
-        Zsc = """
-            class ZMAU_{0[0]}:ZArmorUpgrade{{
-                override
-                ZUpgrade Init(){{
-                    Priority={0[1]};
-                    Strain={0[2]};
-                    return super.Init();
-                }}
-            }}
-        """.format(
-            MAUpgrade
-        )
-        FullFile = Zsc + FullFile
-    print("    Inserting Marine Upgrades:", end=" ")
-    FullFile = FullFile.replace("$ZMAUpgrades", str(MAUpgrades)[1:-1])
-    print("Successful")
-
-    # Templates
     # TODO: C-style preprocessing
-    print("  Generics:")
-
-    # Dictionary
-    print("    ZDictionary")
-    Template = open("ZSCRIPT/TEMPLATE/DICTIONARY.zsc").read()
-    Pattern = re.compile("Map\\s*<\\s*(\\w+)\\s*,\\s*(\\w+)\\s*>")
-    Call = Pattern.search(FullFile)
-    while Call:
-        print("      " + Call.group(1))
-        FullFile += Template.replace("@KeyType", Call.group(1)).replace(
-            "@ValType", Call.group(2)
-        )
-        FullFile = re.sub(
-            "Map\\s*<\\s*" + Call.group(1) + "\\s*,\\s*" + Call.group(2) + "\\s*>",
-            "ZDictionary_" + Call.group(1) + "_" + Call.group(2),
-            FullFile,
-            flags=re.IGNORECASE,
-        )
-        Call = Pattern.search(FullFile)
 
     # End
     return FullFile
--- preprocess.py
+++ preprocess.py
@@ -2,34 +2,30 @@ import re
 
 from strip import ZStript
 
-ConfigPattern = re.compile('"(.+)"\\s*(\\s|,)\\s*"(.+)"')
+IncludePattern = re.compile("\"(.+)\"\\s*", re.IGNORECASE)
+
+ConfigPattern = re.compile(
+    "\"(.+)\"\\s*(\\s|,)\\s*\"(.+)\"", re.IGNORECASE)
 
 
-def ZPreprocess(FullFile, Data):
-    FullFile = ZPConfigBlocks(FullFile)
+def ZPreprocess(FullFile):
     Defines = {}
     Macros = {
         "include": ZPInclude,
-        "config": ZPConfig,
         "define": ZPDefine,
         "undef": ZPUndef,
         "if": ZPIf,
-        "ifn": ZPIfN,
+        "ifdef": ZPIfDef,
+        "ifndef": ZPIfNDef
     }
     Pattern = re.compile("#(\\w+)(\\s+(.*))?")
     Call = Pattern.search(FullFile)
     while Call:
-        if (
-            Call.group(1) == "endif"
-            or Call.group(1) == "region"
-            or Call.group(1) == "endregion"
-        ):
-            FullFile = re.sub(Call.group(0), "", FullFile)
-        Result = Macros[Call.group(1)](FullFile, Call.group(3), Data, Defines)
+        Result = Macros[Call.group(1).lower()](
+            FullFile, Call.group(3), Defines)
         if Result:
-            FullFile = re.sub(
-                "#" + Call.group(1) + "\\s+" + Result[0], Result[1] + "\n", FullFile
-            )
+            FullFile = re.sub("#"+Call.group(1)+"\\s+" +
+                              Result[0], Result[1]+"\n", FullFile)
         else:
             FullFile = re.sub(Call.group(0), "", FullFile)
         Call = Pattern.search(FullFile)
@@ -37,55 +33,30 @@ def ZPreprocess(FullFile, Data):
     return FullFile
 
 
-def ZPConfigBlocks(FullFile):
-    # Config Blocks
-    Pattern = re.compile("\\[Config\\](\\s*){([^}]*)}", re.DOTALL)
-    Call = Pattern.search(FullFile)
-    while Call:
-        Sections = Call.group(2).split(",")
-        Replacement = ""
-        for Section in Sections:
-            Components = re.sub("\\s+", " ", Section).split(":")
-            Replacement += "const {}=#config {};\n".format(
-                Components[0].replace('"', ""), Components[1].replace(".", '","')
-            )
-        FullFile = re.sub(
-            "\\[Config\\](\\s*){" + Call.group(2) + "}", Replacement, FullFile
-        )
-        Call = Pattern.search(FullFile)
-    return FullFile
+def ZPInclude(FullFile, Args, Defines):
+    return (Args, ZStript(open(IncludePattern.match(Args.strip()).group(1)).read()))
 
 
-IncludePattern = re.compile('"(.+)"\\s*')
-
-
-def ZPInclude(FullFile, Args, Data, Defines):
-    return (
-        Args,
-        ZPConfigBlocks(
-            ZStript(open(IncludePattern.match(Args.strip()).group(1)).read())
-        ),
-    )
-
-
-def ZPConfig(FullFile, Args, Data, Defines):
+def ZPConfig(FullFile, Args, Defines):
     Call = ConfigPattern.match(Args.strip())
     return (Call.group(0), Data["CONFIG"][Call.group(1)][Call.group(3)])
 
 
-def ZPDefine(FullFile, Args, Data, Defines):
+def ZPDefine(FullFile, Args, Defines):
     Defines[Args] = True
 
 
-def ZPUndef(FullFile, Args, Data, Defines):
-    if Args in Defines:
-        del Defines[Args]
+def ZPUndef(FullFile, Args, Defines):
+    Defines[Args] = False
 
 
-def ZPIf(FullFile, Args, Data, Defines):
-    if Args not in Defines:
-        FullFile = re.sub("#if\\s+" + Args, "", FullFile)
+def ZPIf(FullFile, Args, Defines):
+    return ""
 
 
-def ZPIfN(FullFile, Args, Data, Defines):
-    return
+def ZPIfDef(FullFile, Args, Defines):
+    return ""
+
+
+def ZPIfNDef(FullFile, Args, Defines):
+    return ""
